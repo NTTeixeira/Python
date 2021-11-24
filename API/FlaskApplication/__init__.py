e7519ee37135b56c20992c7144125533031444db
Nathan Siman Teixeira <64165617+NTTeixeira@users.noreply.github.com>
2021-11-24 12:34:57 -0300

    Update __init__.py

--- API/FlaskApplication/__init__.py
+++ API/FlaskApplication/__init__.py
@@ -4,4 +4,4 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'Thisisasecret'
 
 
-from app.controllers import default
+from FlaskApplication.controllers import default
